296af5940d39caabc84c4c79b5eec4a9b1bc83cc
Rohail Shah <rohails786@gmail.com>
2022-04-24 07:51:22 -0400

    first commit

---
+++ Server/Config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SessionSecret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://127.0.0.1/contacts";
+exports.RemoteURI = "mongodb+srv://rohail:<Xi6idyVW9WbnZVc0>@cluster0.8ilr0.mongodb.net/contacts?retryWrites=true&w=majority";
+exports.HostName = "MongoDB Altas";
+exports.SessionSecret = "WEBD6201SessionSecret";
+//# sourceMappingURL=db.js.map
---
+++ Server/Config/db.ts
@@ -0,0 +1,5 @@
+export const LocalURI = "mongodb://127.0.0.1/contacts";
+export const RemoteURI = "mongodb+srv://rohail:<Xi6idyVW9WbnZVc0>@cluster0.8ilr0.mongodb.net/contacts?retryWrites=true&w=majority"
+export const HostName = "MongoDB Altas";
+//export const HostName = "localhost";
+export const SessionSecret = "WEBD6201SessionSecret";
