55e84712194c4e7f4ac8a173c28178652a8aa5ff
Rohail Shah <rohails786@gmail.com>
2022-04-24 19:57:51 -0400

    first commit

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,5 @@
 export const LocalURI = "mongodb://127.0.0.1/contacts";
+//export const RemoteURI ="mongodb+srv://thomas:lEGPurM5wnM3fh8z@cluster0.c5n0u.mongodb.net/contacts?retryWrites=true&w=majority"
 export const RemoteURI = "mongodb+srv://rohail:<Xi6idyVW9WbnZVc0>@cluster0.8ilr0.mongodb.net/contacts?retryWrites=true&w=majority"
 export const HostName = "MongoDB Altas";
 //export const HostName = "localhost";
